7017ef5f2df234a01639f1caa4d135f7bcb414a5
Andrzej Korolczuk <niepowiem@gmail.com>
2017-05-18 19:29:30 +0100

    commited unity project

---
+++ client/Assets/Scripts/SocketComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.Net;
+
+public class SocketComponent : MonoBehaviour {
+
+	[SerializeField]
+	private string socketServerUrl;
+
+	private Socket socket ;
+
+	private void Awake()
+	{
+		socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
